8fd544049c5362e12c2099e60a67d71dffd724e0
singh-kulwant <singh.kulwant@gmx.com>
2022-05-10 21:30:56 +0200

    05-10: h2 db impl

---
+++ src/main/java/com/kulsin/Book.java
@@ -0,0 +1,35 @@
+package com.kulsin;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Book(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+}
---
+++ src/main/java/com/kulsin/H2DBExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kulsin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DBExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DBExampleApplication.class, args);
+	}
+
+}
